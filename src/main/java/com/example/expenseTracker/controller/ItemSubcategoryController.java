577baf2f7efadcd32672e54d8ddc341ba27b2004
fsv2860 <47431198+fsv2860@users.noreply.github.com>
2022-05-27 23:02:50 -0600

    Item subcategory endpoints implemented
    Endpoints to create, update, delete and search item subcategory have been added.
    Controllers and services for the endpoints have been implemented.

---
+++ src/main/java/com/example/expenseTracker/controller/ItemSubcategoryController.java
@@ -0,0 +1,74 @@
+package com.example.expenseTracker.controller;
+
+import com.example.expenseTracker.dto.request.ItemSubcategoryRequest;
+import com.example.expenseTracker.dto.response.ItemSubcategoryResponse;
+import com.example.expenseTracker.exception.ItemSubcategoryInUseException;
+import com.example.expenseTracker.exception.ItemSubcategoryNotFoundException;
+import com.example.expenseTracker.service.ItemSubcategoryService;
+import com.example.expenseTracker.service.ResponseService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("${api.base.url}/item-subcategory")
+@AllArgsConstructor
+public class ItemSubcategoryController {
+    private final ItemSubcategoryService itemSubcategoryService;
+    private final ResponseService responseService;
+    @PostMapping
+    public ResponseEntity create(@RequestBody ItemSubcategoryRequest itemSubcategoryRequest){
+        HttpStatus httpStatus = null;
+        try {
+            httpStatus = HttpStatus.CREATED;
+            return new ResponseEntity<>(itemSubcategoryService.create(itemSubcategoryRequest),httpStatus);
+        }catch(Exception e){
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }
+    }
+    @PutMapping
+    public ResponseEntity update(@RequestBody ItemSubcategoryRequest itemSubcategoryRequest){
+        HttpStatus httpStatus = null;
+        try {
+            httpStatus = HttpStatus.OK;
+            return new ResponseEntity<>(itemSubcategoryService.update(itemSubcategoryRequest), httpStatus);
+        }catch (ItemSubcategoryNotFoundException e){
+            httpStatus = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }
+        catch(Exception e){
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(name = "id")Long id){
+        HttpStatus httpStatus = null;
+        try {
+            httpStatus = HttpStatus.OK;
+            itemSubcategoryService.delete(id);
+            return new ResponseEntity<>(responseService.delete("item-subcategory", httpStatus), httpStatus);
+        }catch (ItemSubcategoryNotFoundException | ItemSubcategoryInUseException e){
+            httpStatus = HttpStatus.BAD_REQUEST;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }catch(Exception e){
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }
+    }
+    @GetMapping
+    public ResponseEntity get(@RequestParam(name = "name", required = false)String name,
+                              @RequestParam(name = "categoryId", required = false)Long categoryId){
+        HttpStatus httpStatus = null;
+        try {
+            httpStatus = HttpStatus.OK;
+            return new ResponseEntity<>(itemSubcategoryService.get(name,categoryId), httpStatus);
+        }
+        catch(Exception e){
+            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
+            return new ResponseEntity<>(responseService.error(e,httpStatus),httpStatus);
+        }
+    }
+}
---
+++ src/main/java/com/example/expenseTracker/service/ItemSubcategoryService.java
@@ -0,0 +1,82 @@
+package com.example.expenseTracker.service;
+
+import com.example.expenseTracker.dto.request.ItemSubcategoryRequest;
+import com.example.expenseTracker.dto.response.ItemSubcategoryResponse;
+import com.example.expenseTracker.exception.ItemCategoryNotFoundException;
+import com.example.expenseTracker.exception.ItemSubcategoryInUseException;
+import com.example.expenseTracker.exception.ItemSubcategoryNotFoundException;
+import com.example.expenseTracker.mapper.ItemSubcategoryMapper;
+import com.example.expenseTracker.model.ItemCategory;
+import com.example.expenseTracker.model.ItemSubcategory;
+import com.example.expenseTracker.repository.ItemCategoryRepository;
+import com.example.expenseTracker.repository.ItemRepository;
+import com.example.expenseTracker.repository.ItemSubcategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class ItemSubcategoryService {
+    private final ItemSubcategoryMapper itemSubcategoryMapper;
+    private final ItemSubcategoryRepository itemSubcategoryRepository;
+    private final ItemCategoryRepository itemCategoryRepository;
+    private final ItemRepository itemRepository;
+    public ItemSubcategoryResponse create(ItemSubcategoryRequest itemSubcategoryRequest){
+        // Map the request, create the subcategory and then return it.
+        ItemSubcategory itemSubcategory = itemSubcategoryRepository.save(itemSubcategoryMapper.mapToEntity(itemSubcategoryRequest));
+        return itemSubcategoryMapper.mapToDto(itemSubcategory);
+    }
+    public ItemSubcategoryResponse update(ItemSubcategoryRequest itemSubcategoryRequest){
+        // Verify that the subcategory exists before updating it.
+        itemSubcategoryRepository.findById(itemSubcategoryRequest.getId()).orElseThrow(()-> new ItemSubcategoryNotFoundException(itemSubcategoryRequest.getId()));
+        // Map the request, create the subcategory and then return it.
+        ItemSubcategory itemSubcategory = itemSubcategoryRepository.save(itemSubcategoryMapper.mapToEntity(itemSubcategoryRequest));
+        return itemSubcategoryMapper.mapToDto(itemSubcategory);
+    }
+    public void delete(Long id){
+        // Find the subcategory.
+        ItemSubcategory itemSubcategory = itemSubcategoryRepository.findById(id).orElseThrow(()-> new ItemSubcategoryNotFoundException(id));
+        // Can't delete subcategory that are being used by an item.
+        if(!itemRepository.findBySubcategory(itemSubcategory).isEmpty()){
+            throw new ItemSubcategoryInUseException(itemSubcategory);
+        };
+        // Delete it.
+        itemSubcategoryRepository.delete(itemSubcategory);
+    }
+    public List<ItemSubcategoryResponse> get(String name, Long categoryId){
+        List <ItemSubcategory> queryResults = null;
+        ItemCategory itemCategory = null;
+        // Search the category, if there's any.
+        if(categoryId != null){
+            itemCategory = itemCategoryRepository.findById(categoryId)
+                    .orElseThrow(()-> new ItemCategoryNotFoundException(categoryId));
+        }
+        // Query section.
+        if(name != null && !name.isEmpty()){
+            // Upper case and remove spaces.
+            name = name.toUpperCase(Locale.ROOT).trim();
+            if(itemCategory != null){
+                // Name and category query.
+                queryResults = itemSubcategoryRepository.findByNameContainsIgnoreCaseAndCategory(name,itemCategory);
+            }else{
+                // Only by name.
+                queryResults = itemSubcategoryRepository.findByNameContainsIgnoreCase(name);
+            }
+        }else if(itemCategory != null){
+            // By category.
+            queryResults = itemSubcategoryRepository.findByCategory(itemCategory);
+        }
+        else {
+            // Return all.
+            queryResults = itemSubcategoryRepository.findAll();
+        }
+        // Map and return the results.
+        return queryResults.stream()
+                .map(itemSubcategoryMapper :: mapToDto)
+                .collect(Collectors.toList());
+    }
+}
